b18756dde5f12575a585ef8555a94a7db68fa990
lyy <844843431749@qq.com>
2023-02-17 12:06:15 +0800

    hot_fix

--- src/main/java/com/atguigu/git/gittest.java
+++ src/main/java/com/atguigu/git/gittest.java
@@ -2,10 +2,12 @@ package com.atguigu.git;
 
 /**
  * @author LYY
- * @create 2023-02-16 22:11
+ *
  */
 public class gittest {
     public static void main(String[] args) {
         System.out.println("hello fuck you!");
+        System.out.println('p');
+        System.out.println("i will");
     }
 }
